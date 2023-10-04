39869007ad1c60f29caf48da8d8bd75c719543cd
Matías Roy Rodriguez <matiasrr980@gmail.com>
2023-10-03 21:30:04 -0300

    feat: add logout

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -54,7 +54,10 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
 
-    if (!token) return of(false);
+    if (!token) {
+      this.logout();
+      return of(false)
+    };
 
 
     const headers = new HttpHeaders()
@@ -73,6 +76,13 @@ export class AuthService {
         )
       )
 
+  }
+
+
+  logout() {
+    localStorage.removeItem('token');
+    this._currentUser.set(null);
+    this._authStatus.set(AuthStatus.notAuthenticated);
   }
 
 }
